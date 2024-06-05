bee6ba1041a87e7059371442f4a1c8038bf6b78c
bakka22 <abobakereltom@gmail.com>
2024-06-05 05:46:47 +0200

    boxes unlocked

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" locked boxes problem solution """
+
+
+def canUnlockAll(boxes):
+    keys = set()
+    opend = []
+    flag = False
+    keys |= set(boxes[0])
+    opend.append(0)
+    newkeys = set()
+    while (len(opend) < len(boxes)):
+        newkeys = set()
+        if len(keys) == 0:
+            break
+        for i in keys:
+            if (i < len(boxes)):
+                if (i not in opend):
+                    opend.append(i)
+                    newkeys |= set(boxes[i])
+                    if len(opend) == len(boxes):
+                        flag = True
+                        break
+        keys = newkeys - keys
+    return flag
