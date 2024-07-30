f38542066d612d05d161d7f17f9c04ae56f46d05
bakka22 <abobakereltom@gmail.com>
2024-07-30 21:42:48 +0200

    S

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,16 +11,16 @@ def canUnlockAll(boxes):
     length = len(boxes)
     used_keys = set([0])
     new_keys = set()
-    opend = 1
-    to_use_keys = set(boxes[0])
+    opend = 0
+    to_use_keys = set([0])
     while len(to_use_keys) != 0:
         for key in to_use_keys:
-            if key < length and key > 0:
+            if key < length and key >= 0:
                 if type(boxes[key]) != list:
                     return False
                 new_keys = new_keys | set(boxes[key])
                 opend += 1
-                #print(f"opend box number: {key}, opend: {opend}")
+                # print(f"opend box number: {key}, opend: {opend}")
         if (opend == length):
             return True
         to_use_keys = new_keys - used_keys
--- 0x01-lockboxes/main.py
+++ 0x01-lockboxes/main.py
@@ -2,18 +2,21 @@
 
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
 
-''' boxes = [[1], [2], [3], [4], []]
+boxes = [[1], [2], [3], [4], []]
 print(canUnlockAll(boxes), "correct: True")
 
 boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
 print(canUnlockAll(boxes), "correct: True")
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes), "correct: False")'''
+print(canUnlockAll(boxes), "correct: False")
 
 boxes = [[1, 10], [2, 5], [4], [3], [3], []]
 print(canUnlockAll(boxes), "correct: True")
-'''
+
+boxes = [[1, 10, 5], [2, 5, 1], [4, 2], [3, 7], [3], [], []]
+print(canUnlockAll(boxes), "correct: False")
+
 boxes = {}
 print(canUnlockAll(boxes), "correct: False")
 
@@ -25,4 +28,4 @@ print(canUnlockAll(boxes), "correct: True")
 
 boxes = [[]]
 print(canUnlockAll(boxes), "correct: True")
-'''
+
