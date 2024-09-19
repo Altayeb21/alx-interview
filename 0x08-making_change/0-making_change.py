8f1399a0db2f95766c9a318de07fc6c7bb86bc88
bakka22 <abobakereltom@gmail.com>
2024-09-19 18:44:02 +0200

    s

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,6 +10,7 @@ def makeChange(coins, total, memo=None):
 
     if memo is None:
         memo = {}
+        coins.sort()
     minimum = -1
     for coin in coins:
         if total - coin >= 0:
@@ -22,5 +23,7 @@ def makeChange(coins, total, memo=None):
                 memo[total - coin] = path
                 if path != -1:
                     minimum = min(minimum, temp) if minimum != -1 else max(minimum, path)
+        else:
+            break
 
     return minimum + 1 if minimum != -1 else minimum
